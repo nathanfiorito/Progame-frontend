bbe3edb11f170f9641e8eb034de13be41827a5ac
Nathan Fiorito <nfiorito64@gmail.com>
2022-12-09 10:57:26 -0300

    ...

--- src/app/dashboard/ranking/ranking.component.ts
+++ src/app/dashboard/ranking/ranking.component.ts
@@ -18,6 +18,7 @@ export class RankingComponent implements OnInit {
   ngOnInit(): void {
     this.userService.getAllUsers().then(response => {
       this.users = response.Data
+      this.users.sort((a, b) => b.Experience - a.Experience);
       this.usersCount = this.users.length;
       this.topUsers = this.users.slice(0,3)
       this.users = this.users.slice(3, this.users.length);
@@ -27,10 +28,9 @@ export class RankingComponent implements OnInit {
 
   getDate(date:Date){
     let auxDate = new Date(date)
-    const today = new Date();
-    const yyyy = today.getFullYear();
-    let mm = (today.getMonth() + 1).toString(); // Months start at 0!
-    let dd = (today.getDate()).toString();
+    const yyyy = auxDate.getFullYear();
+    let mm = (auxDate.getMonth() + 1).toString(); // Months start at 0!
+    let dd = (auxDate.getDate()).toString();
 
     if (parseInt(dd) < 10) dd = '0' + dd;
     if (parseInt(mm) < 10) mm = '0' + mm;
